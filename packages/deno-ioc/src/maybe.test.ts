90bfd80ffa35258ba88fa7f3d5dd04e513c6beb9
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2020-04-26 17:49:19 +0200

    Maybe class + some tests

--- packages/deno-ioc/src/maybe.test.ts
+++ packages/deno-ioc/src/maybe.test.ts
@@ -1,13 +1,37 @@
-import { assert, assertEquals } from "std/testing/asserts.ts";
+import { expect } from "../../deno-expect/mod.ts";
+import { Maybe } from "./maybe.ts";
 
-import { Maybe } from "@/maybe.ts";
+Deno.test("Maybe::some constructor", async () => {
+  const m = Maybe.some(15);
+  expect(m.value()).toBe(15);
+});
+Deno.test("Maybe::none constructor", async () => {
+  const m = Maybe.none();
 
-Deno.test("Maybe: ok constructor", async () => {
-  const m = Maybe.ok(15);
-  let called = false;
-  m.map((value) => {
-    assertEquals(value, 15, "Wrong value");
-    called = true;
-  });
-  assert(called, "Maybe.map callback not called");
+  expect(() => m.value()).toThrow();
+});
+
+Deno.test("Maybe::map", async () => {
+  expect(Maybe.some(12).map((n) => n * 5)
+    .value()).toEqual(60);
+  expect(Maybe.some("some text").map((s) => s.toUpperCase())
+    .value()).toEqual("SOME TEXT");
+});
+
+Deno.test("Maybe::map2", async () => {
+  const some5 = Maybe.some(5);
+  const none = Maybe.none<number>();
+  const sum = (a: any, b: any) => a + b;
+
+  for (
+    const [a, b, fn] of [
+      [none, none, Maybe.isNone],
+      [none, some5, Maybe.isNone],
+      [some5, none, Maybe.isNone],
+      [some5, some5, Maybe.isSome],
+    ] as any
+  ) {
+    var v = Maybe.map2(a, b, sum);
+    expect(fn(v)).toBe(true);
+  }
 });
--- packages/deno-ioc/src/maybe.ts
+++ packages/deno-ioc/src/maybe.ts
@@ -1,5 +1,5 @@
 export class Maybe<T> {
-  static ok<T>(value: T): Maybe<T> {
+  static some<T>(value: T): Maybe<T> {
     return new Maybe(value);
   }
 
@@ -7,22 +7,51 @@ export class Maybe<T> {
     return new Maybe(null as any as T);
   }
 
+  static isSome<T>(m: Maybe<T>): boolean {
+    return m.isSome();
+  }
+
+  static isNone<T>(m: Maybe<T>): boolean {
+    return m.isNone();
+  }
+
   static map<T, R>(m: Maybe<T>, fn: (value: T) => R): Maybe<R> {
     if (m._value !== null) {
-      return Maybe.ok(fn(m._value));
+      return Maybe.some(fn(m._value));
     }
     return Maybe.none();
   }
 
-  private _hasValue: boolean;
+  static map2<T1, T2, R>(
+    m1: Maybe<T1>,
+    m2: Maybe<T2>,
+    fn: (v1: T1, v2: T2) => R,
+  ): Maybe<R> {
+    if (m1.isNone() || m2.isNone()) {
+      return Maybe.none();
+    }
+    return Maybe.some(fn(m1._value!, m2._value!));
+  }
+
+  private readonly _hasValue: boolean;
   private constructor(
     private readonly _value: T | null,
   ) {
     this._hasValue = _value !== null;
   }
 
-  public isOk(): boolean {
+  public value() {
+    if (!this._hasValue) {
+      throw new Error("Maybe.none value accessed");
+    }
+    return this._value;
+  }
+
+  public isSome(): boolean {
     return this._hasValue;
+  }
+  public isNone(): boolean {
+    return !this._hasValue;
   }
 
   public map<R>(fn: (value: T) => R): Maybe<R> {
