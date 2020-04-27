6cd0142e76ed872c90fd4fce2ba38f0ceebddefa
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2020-04-27 12:34:33 +0200

    Simple container

---
+++ packages/ioc/src/flow1.test.ts
@@ -0,0 +1,30 @@
+import { expect } from 'x/expect/mod.ts'
+
+import { suite } from './test_suite.ts'
+import { Container } from './container.ts'
+
+const it = suite("Flow1")
+
+const container = new Container()
+
+class Example { }
+
+it("can register a class", () => {
+    container.register(Example)
+
+    class LocalClass { }
+    container.register(LocalClass)
+})
+it("can inject a class", () => {
+    expect(container.resolve(Example)).not.toBeNull()
+    class LocalClass { }
+    expect(container.resolve(LocalClass)).toBeNull()
+})
+
+it("can override a registration", () => {
+    expect(container.resolve(Example)).not.toBeNull()
+    container.override(Example, {
+        useValue: 5
+    })
+    expect(container.resolve(Example)).toBe(5)
+})
