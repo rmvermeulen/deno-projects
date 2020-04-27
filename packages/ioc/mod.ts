b8e0cfe435b9cebd2b8a2778008b109afce6471f
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2020-04-27 14:43:13 +0200

    ioc: filenames, folder structure

---
+++ packages/ioc/mod.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env deno
+import { assert } from 'std/testing/asserts.ts'
+
+import "reflect-metadata";
+
+declare const Reflect: {
+    metadata(): void
+    defineMetadata(): void
+    getMetadata(): void
+}
+
+
+assert(typeof Reflect['metadata'] === 'function', "Reflect::metadata missing")
+assert(typeof Reflect['getMetadata'] === 'function', "Reflect::getMetadata missing")
+assert(typeof Reflect['defineMetadata'] === 'function', "Reflect::defineMetadata missing")
+
+
+import { Maybe } from "./src/maybe.ts";
+
+export * as Decorators from './src/decorators.ts'
+
+export { Application } from './src/Application.ts'
--- packages/ioc/src/decorators.ts
+++ packages/ioc/src/decorators.ts
@@ -1,12 +1,17 @@
-export function factory(dfn: (...args: any[]) => ClassDecorator) {
-  return function (...args: any[]) {
-    const decorator = dfn(...args);
-    return function (_class) {
-      return decorator(_class);
-    };
-  };
-}
+import 'npm/reflect-metadata/Reflect.js'
 
-export const Injectable: ClassDecorator = (_class) => {
-  return _class;
-};
+
+export const factory =
+  (dfn: (...args: any[]) => ClassDecorator) => {
+    return (...args: any[]) => {
+      const decorator = dfn(...args);
+      return <TF extends Function>(_class: TF) =>
+        decorator(_class)
+    };
+  }
+
+export const Injectable: ClassDecorator =
+  <TF extends Function>(_class: TF) => {
+    // Reflect.
+    return _class;
+  };
